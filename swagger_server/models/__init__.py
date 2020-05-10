81a77fd32a970205894ab9e4a1286e0d0bd99e7c
RichardBlackEye <62513655+RichardBlackEye@users.noreply.github.com>
2020-05-10 12:47:30 +0100

    Add files via upload

---
+++ swagger_server/models/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.api_response import ApiResponse
+from swagger_server.models.country import Country
+from swagger_server.models.indicator import Indicator
+from swagger_server.models.tag import Tag
