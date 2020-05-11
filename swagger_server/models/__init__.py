b8749769e59ef46b5d4ffc76768cbc980914cde9
RichardBlackEye <62513655+RichardBlackEye@users.noreply.github.com>
2020-05-11 11:36:34 +0100

    Update __init__.py

--- swagger_server/models/__init__.py
+++ swagger_server/models/__init__.py
@@ -6,4 +6,4 @@ from __future__ import absolute_import
 from swagger_server.models.api_response import ApiResponse
 from swagger_server.models.country import Country
 from swagger_server.models.indicator import Indicator
-from swagger_server.models.tag import Tag
+
