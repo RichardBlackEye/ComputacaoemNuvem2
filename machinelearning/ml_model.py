9f062907de8f7bcb61b1f7f39d8f9b62fdc938f8
WinGamingPT <31018077+WinGamingPT@users.noreply.github.com>
2020-05-25 12:26:42 +0100

    Add files via upload

---
+++ machinelearning/ml_model.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.feature import VectorAssembler
+
+spark = SparkSession.builder.getOrCreate()
+
+data = spark.read.csv('./boston_housing.csv', header=True, inferSchema=True)
+
+# create features vector
+feature_columns = data.columns[:-1] # here we omit the final column
+
+assembler = VectorAssembler(inputCols=feature_columns,outputCol="features")
+
+data_2 = assembler.transform(data)
+
+algo = RandomForestClassifier(featuresCol='features', labelCol='Crisis')
+model = algo.fit(data) 
