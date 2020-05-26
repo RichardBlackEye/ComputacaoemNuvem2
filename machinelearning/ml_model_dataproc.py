c263ccfe8030c38b9182d2831371585ac59ba3b0
WinGamingPT <31018077+WinGamingPT@users.noreply.github.com>
2020-05-27 00:23:34 +0100

    Add files via upload

---
+++ machinelearning/ml_model_dataproc.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.feature import VectorAssembler
+
+spark = SparkSession.builder.getOrCreate()
+
+
+data= spark.read.format("bigquery").option("table", "germany_regression.germany").load()
+
+
+
+
+
+# create features vector
+feature_columns =['Value'] # here we omit the final column
+
+
+data2 = clean_data.rdd.map(feature_colums).toDF(["label","features"])
+
+data2.cache()
+
+algo = RandomForestClassifier(featuresCol='features', labelCol='Crisis')
+model = algo.fit(data_2) 
