d64a42010af0983889a711eba8412dc7663f01ac
Sonia Portugal <>
2020-05-27 12:41:32 +0100

    zip com ficheiros moodle

--- machinelearning/ml_model.py
+++ machinelearning/ml_model.py
@@ -1,23 +1,20 @@
+from pyspark.mllib.linalg import Vectors
 from pyspark.sql import SparkSession
 from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.feature import VectorAssembler
 
 spark = SparkSession.builder.getOrCreate()
 
-data = spark.read.csv('./indicators3.csv', header=True, inferSchema=True)
-
-
+data = spark.read.csv('./germany.csv', header=True, inferSchema=True)
 
 # create features vector
-feature_columns =['Value'] # here we omit the final column
+feature_columns = data.columns[:-1] # here we omit the final column
+feature = feature_columns
 
 
 assembler = VectorAssembler(inputCols=feature_columns,outputCol="features")
 
 data_2 = assembler.transform(data)
 
-
-
-
-algo = RandomForestClassifier(featuresCol='features', labelCol='Crisis')
-model = algo.fit(data_2) 
+algo = RandomForestClassifier(featuresCol="features", labelCol="Crisis")
+model = algo.fit(data) 
