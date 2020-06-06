4f2a09fbddc3a219222f64b72fe8a12c49a243b5
RichardBlackEye <62513655+RichardBlackEye@users.noreply.github.com>
2020-06-06 14:42:58 +0100

    Add files via upload

--- machinelearning/ml_prediction.py
+++ machinelearning/ml_prediction.py
@@ -1,12 +1,20 @@
 from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.classification import RandomForestClassificationModel
 from pyspark.ml.feature import VectorAssembler
+
+# Server
+import uvicorn              #connexion
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
 sc = SparkContext()
 
 spark = SparkSession(sc)
+
+model = RandomForestClassificationModel.load("gs://germl/model")
 
 data= spark.read.format("bigquery").option("table", "germany_regression.germany").load()
 
@@ -19,8 +27,13 @@ assembler = VectorAssembler(inputCols=feature_columns,outputCol="features")
 
 data_2 = assembler.transform(data)
 
-algo = RandomForestClassifier(featuresCol='features', labelCol='Crise')
-model = algo.fit(data_2) 
-model.save("gs://germl/model")
 
+@app.post("/predict")
 
+def predict():
+
+    predictions = model.transform(data_2) 
+
+    #predictions.select(['Crise','prediction', 'probability']).show()
+
+    return (predictions.select(['Crise','prediction', 'probability']))
